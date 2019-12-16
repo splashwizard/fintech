71737add2f5612cb75a6319e5d5618725fd897ff
admiral <admiral1ap0901@gmail.com>
2019-12-16 15:51:28 +0800

    v1

--- resources/views/account/deposit.blade.php
+++ resources/views/account/deposit.blade.php
@@ -26,14 +26,14 @@
             </div>
 
 {{--            <div class="form-group">--}}
-{{--                {!! Form::label('operation_date', __( 'messages.date' ) .":*") !!}--}}
-{{--                <div class="input-group date" id='od_datetimepicker'>--}}
-{{--                  {!! Form::text('operation_date', 0, ['class' => 'form-control', 'required','placeholder' => __( 'messages.date' ) ]); !!}--}}
-{{--                  <span class="input-group-addon">--}}
-{{--                    <span class="glyphicon glyphicon-calendar"></span>--}}
-{{--                  </span>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+        {{--                {!! Form::label('operation_date', __( 'messages.date' ) .":*") !!}--}}
+        {{--                <div class="input-group date" id='od_datetimepicker'>--}}
+        {{--                  {!! Form::text('operation_date', 0, ['class' => 'form-control', 'required','placeholder' => __( 'messages.date' ) ]); !!}--}}
+        {{--                  <span class="input-group-addon">--}}
+        {{--                    <span class="glyphicon glyphicon-calendar"></span>--}}
+        {{--                  </span>--}}
+        {{--                </div>--}}
+        {{--            </div>--}}
 
             <div class="form-group">
                 {!! Form::label('note', __( 'brand.note' )) !!}
