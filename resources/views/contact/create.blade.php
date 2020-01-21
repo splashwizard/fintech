d775557de43b9f4afd1a5851ffe2af310694cf02
admiral <admiral1ap0901@gmail.com>
2020-01-21 16:53:39 +0800

    v1

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -15,19 +15,7 @@
 
     <div class="modal-body">
       <div class="row">
-
-{{--      <div class="col-md-6 contact_type_div">--}}
-{{--        <div class="form-group">--}}
-{{--            {!! Form::label('type', __('contact.contact_type') . ':*' ) !!}--}}
-{{--            <div class="input-group">--}}
-{{--                <span class="input-group-addon">--}}
-{{--                    <i class="fa fa-user"></i>--}}
-{{--                </span>--}}
-{{--                {!! Form::select('type', $types, null , ['class' => 'form-control', 'id' => 'contact_type','placeholder' => __('messages.please_select'), 'required']); !!}--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--      </div>--}}
-      <div class="col-md-6">
+      <div class="col-md-4">
         <div class="form-group">
             {!! Form::label('name', __('contact.name') . ':*') !!}
             <div class="input-group">
@@ -38,41 +26,6 @@
             </div>
         </div>
       </div>
-      <div class="clearfix"></div>
-{{--      <div class="col-md-4 supplier_fields">--}}
-{{--        <div class="form-group">--}}
-{{--            {!! Form::label('supplier_business_name', __('business.business_name') . ':*') !!}--}}
-{{--            <div class="input-group">--}}
-{{--                <span class="input-group-addon">--}}
-{{--                    <i class="fa fa-briefcase"></i>--}}
-{{--                </span>--}}
-{{--                {!! Form::text('supplier_business_name', null, ['class' => 'form-control', 'required', 'placeholder' => __('business.business_name')]); !!}--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--      </div>--}}
-{{--      <div class="col-md-4">--}}
-{{--        <div class="form-group">--}}
-{{--            {!! Form::label('contact_id', __('lang_v1.contact_id') . ':') !!}--}}
-{{--            <div class="input-group">--}}
-{{--                <span class="input-group-addon">--}}
-{{--                    <i class="fa fa-id-badge"></i>--}}
-{{--                </span>--}}
-{{--                {!! Form::text('contact_id', null, ['class' => 'form-control','placeholder' => __('lang_v1.contact_id')]); !!}--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--      </div>--}}
-        <div class="col-md-4">
-          <div class="form-group">
-              {!! Form::label('tax_number', __('contact.tax_no') . ':') !!}
-              <div class="input-group">
-                  <span class="input-group-addon">
-                      <i class="fa fa-info"></i>
-                  </span>
-                  {!! Form::text('tax_number', null, ['class' => 'form-control', 'placeholder' => __('contact.tax_no')]); !!}
-              </div>
-          </div>
-        </div>
-
         <div class="col-md-4">
           <div class="form-group">
               {!! Form::label('opening_balance', __('lang_v1.opening_balance') . ':') !!}
@@ -82,19 +35,6 @@
                   </span>
                   {!! Form::text('opening_balance', 0, ['class' => 'form-control input_number']); !!}
               </div>
-          </div>
-        </div>
-        <div class="clearfix"></div>
-
-        <div class="col-md-4">
-          <div class="form-group">
-            <div class="multi-input">
-              {!! Form::label('pay_term_number', __('contact.pay_term') . ':') !!} @show_tooltip(__('tooltip.pay_term'))
-              <br/>
-              {!! Form::number('pay_term_number', null, ['class' => 'form-control width-40 pull-left', 'placeholder' => __('contact.pay_term')]); !!}
-
-              {!! Form::select('pay_term_type', ['months' => __('lang_v1.months'), 'days' => __('lang_v1.days')], '', ['class' => 'form-control width-60 pull-left','placeholder' => __('messages.please_select')]); !!}
-            </div>
           </div>
         </div>
         
@@ -107,18 +47,6 @@
                   </span>
                   {!! Form::select('customer_group_id', $customer_groups, '', ['class' => 'form-control']); !!}
               </div>
-          </div>
-        </div>
-        <div class="col-md-4 customer_fields">
-          <div class="form-group">
-              {!! Form::label('credit_limit', __('lang_v1.credit_limit') . ':') !!}
-              <div class="input-group">
-                  <span class="input-group-addon">
-                      <i class="fa fa-money"></i>
-                  </span>
-                  {!! Form::text('credit_limit', null, ['class' => 'form-control input_number']); !!}
-              </div>
-              <p class="help-block">@lang('lang_v1.credit_limit_help')</p>
           </div>
         </div>
       <div class="col-md-12">
@@ -146,74 +74,7 @@
             </div>
         </div>
       </div>
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('alternate_number', __('contact.alternate_contact_number') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-phone"></i>
-                </span>
-                {!! Form::text('alternate_number', null, ['class' => 'form-control', 'placeholder' => __('contact.alternate_contact_number')]); !!}
-            </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('landline', __('contact.landline') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-phone"></i>
-                </span>
-                {!! Form::text('landline', null, ['class' => 'form-control', 'placeholder' => __('contact.landline')]); !!}
-            </div>
-        </div>
-      </div>
       <div class="clearfix"></div>
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('city', __('business.city') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-map-marker"></i>
-                </span>
-                {!! Form::text('city', null, ['class' => 'form-control', 'placeholder' => __('business.city')]); !!}
-            </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('state', __('business.state') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-map-marker"></i>
-                </span>
-                {!! Form::text('state', null, ['class' => 'form-control', 'placeholder' => __('business.state')]); !!}
-            </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('country', __('business.country') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-globe"></i>
-                </span>
-                {!! Form::text('country', null, ['class' => 'form-control', 'placeholder' => __('business.country')]); !!}
-            </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('landmark', __('business.landmark') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-map-marker"></i>
-                </span>
-                {!! Form::text('landmark', null, ['class' => 'form-control', 
-                'placeholder' => __('business.landmark')]); !!}
-            </div>
-        </div>
-      </div>
       <div> 
       <div class="clearfix"></div>
       <div class="col-md-12">
--- resources/views/contact/edit.blade.php
+++ resources/views/contact/edit.blade.php
@@ -11,19 +11,7 @@
     <div class="modal-body">
 
       <div class="row">
-
-{{--        <div class="col-md-6">--}}
-{{--          <div class="form-group">--}}
-{{--              {!! Form::label('type', __('contact.contact_type') . ':*' ) !!}--}}
-{{--              <div class="input-group">--}}
-{{--                  <span class="input-group-addon">--}}
-{{--                      <i class="fa fa-user"></i>--}}
-{{--                  </span>--}}
-{{--                  {!! Form::select('type', $types, $contact->type, ['class' => 'form-control', 'id' => 'contact_type','placeholder' => __('messages.please_select'), 'required']); !!}--}}
-{{--              </div>--}}
-{{--          </div>--}}
-{{--        </div>--}}
-        <div class="col-md-6">
+        <div class="col-md-4">
           <div class="form-group">
               {!! Form::label('name', __('contact.name') . ':*') !!}
               <div class="input-group">
@@ -31,42 +19,6 @@
                       <i class="fa fa-user"></i>
                   </span>
                   {!! Form::text('name', $contact->name, ['class' => 'form-control','placeholder' => __('contact.name'), 'required']); !!}
-              </div>
-          </div>
-        </div>
-        <div class="clearfix"></div>
-        <div class="col-md-4 supplier_fields">
-          <div class="form-group">
-              {!! Form::label('supplier_business_name', __('business.business_name') . ':*') !!}
-              <div class="input-group">
-                  <span class="input-group-addon">
-                      <i class="fa fa-briefcase"></i>
-                  </span>
-                  {!! Form::text('supplier_business_name', 
-                  $contact->supplier_business_name, ['class' => 'form-control', 'required', 'placeholder' => __('business.business_name')]); !!}
-              </div>
-          </div>
-        </div>
-{{--        <div class="col-md-4">--}}
-{{--          <div class="form-group">--}}
-{{--              {!! Form::label('contact_id', __('lang_v1.contact_id') . ':') !!}--}}
-{{--              <div class="input-group">--}}
-{{--                  <span class="input-group-addon">--}}
-{{--                      <i class="fa fa-id-badge"></i>--}}
-{{--                  </span>--}}
-{{--                  <input type="hidden" id="hidden_id" value="{{$contact->id}}">--}}
-{{--                  {!! Form::text('contact_id', $contact->contact_id, ['class' => 'form-control','placeholder' => __('lang_v1.contact_id')]); !!}--}}
-{{--              </div>--}}
-{{--          </div>--}}
-{{--        </div>--}}
-        <div class="col-md-4">
-          <div class="form-group">
-              {!! Form::label('tax_number', __('contact.tax_no') . ':') !!}
-              <div class="input-group">
-                  <span class="input-group-addon">
-                      <i class="fa fa-info"></i>
-                  </span>
-                  {!! Form::text('tax_number', $contact->tax_number, ['class' => 'form-control', 'placeholder' => __('contact.tax_no')]); !!}
               </div>
           </div>
         </div>
@@ -81,19 +33,7 @@
               </div>
           </div>
         </div>
-          <div class="clearfix"></div>
 
-        <div class="col-md-4">
-          <div class="form-group">
-            <div class="multi-input">
-              {!! Form::label('pay_term_number', __('contact.pay_term') . ':') !!} @show_tooltip(__('tooltip.pay_term'))
-              <br/>
-              {!! Form::number('pay_term_number', $contact->pay_term_number, ['class' => 'form-control width-40 pull-left', 'placeholder' => __('contact.pay_term')]); !!}
-
-              {!! Form::select('pay_term_type', ['months' => __('lang_v1.months'), 'days' => __('lang_v1.days')], $contact->pay_term_type, ['class' => 'form-control width-60 pull-left','placeholder' => __('messages.please_select')]); !!}
-            </div>
-          </div>
-        </div>
         
         <div class="col-md-4 customer_fields">
           <div class="form-group">
@@ -106,18 +46,7 @@
               </div>
           </div>
         </div>
-        <div class="col-md-4 customer_fields">
-          <div class="form-group">
-              {!! Form::label('credit_limit', __('lang_v1.credit_limit') . ':') !!}
-              <div class="input-group">
-                  <span class="input-group-addon">
-                      <i class="fa fa-money"></i>
-                  </span>
-                  {!! Form::text('credit_limit', $contact->credit_limit, ['class' => 'form-control input_number']); !!}
-              </div>
-              <p class="help-block">@lang('lang_v1.credit_limit_help')</p>
-          </div>
-        </div>
+
       <div class="col-md-12">
         <hr/>
       </div>
@@ -143,74 +72,8 @@
             </div>
         </div>
       </div>
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('alternate_number', __('contact.alternate_contact_number') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-phone"></i>
-                </span>
-                {!! Form::text('alternate_number', $contact->alternate_number, ['class' => 'form-control', 'placeholder' => __('contact.alternate_contact_number')]); !!}
-            </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('landline', __('contact.landline') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-phone"></i>
-                </span>
-                {!! Form::text('landline', $contact->landline, ['class' => 'form-control', 'placeholder' => __('contact.landline')]); !!}
-            </div>
-        </div>
-      </div>
-      <div class="clearfix"></div>
 
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('city', __('business.city') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-map-marker"></i>
-                </span>
-                {!! Form::text('city', $contact->city, ['class' => 'form-control', 'placeholder' => __('business.city')]); !!}
-            </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('state', __('business.state') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-map-marker"></i>
-                </span>
-                {!! Form::text('state', $contact->state, ['class' => 'form-control', 'placeholder' => __('business.state')]); !!}
-            </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('country', __('business.country') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-globe"></i>
-                </span>
-                {!! Form::text('country', $contact->country, ['class' => 'form-control', 'placeholder' => __('business.country')]); !!}
-            </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="form-group">
-            {!! Form::label('landmark', __('business.landmark') . ':') !!}
-            <div class="input-group">
-                <span class="input-group-addon">
-                    <i class="fa fa-map-marker"></i>
-                </span>
-                {!! Form::text('landmark', $contact->landmark, ['class' => 'form-control', 'placeholder' => __('business.landmark')]); !!}
-            </div>
-        </div>
-      </div>
+      <div class="clearfix"></div>
       <div class="clearfix"></div>
       <div class="col-md-12">
         <hr/>
