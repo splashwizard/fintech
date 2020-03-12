b3e84f3e5658974e724697c7c28671b1256fc8ef
jinz <admiral1ap0901@gmail.com>
2020-03-12 01:47:59 -0700

    v1

---
+++ database/migrations/2020_03_11_163247_add_bank_details_to_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBankDetailsToContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            //
+            $table->text('bank_details')->nullable()->after('remark');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            //
+        });
+    }
+}
