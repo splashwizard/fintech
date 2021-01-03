eff8fbfcd59fbc2fd55c3a921822ae0fdb7cdf83
kizzy0216 <m.kizzy.up@gmail.com>
2021-01-02 21:06:47 -0600

    v1

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,21 +15,24 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-//        return $next($request)
-//            ->header('Access-Control-Allow-Origin', '*');
-        header("Access-Control-Allow-Origin: *");
-        // ALLOW OPTIONS METHOD
-        $headers = [
-            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
-        ];
-        if($request->getMethod() == "OPTIONS") {
-            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
-            return Response::make('OK', 200, $headers);
-        }
-        $response = $next($request);
-        foreach($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
+        return $next($request);
+        // ->header('Access-Control-Allow-Origin', '*');
+        // ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        // ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
+        // ->header('Access-Control-Allow-Credentials',' true');
+        // header("Access-Control-Allow-Origin: *");
+        // // ALLOW OPTIONS METHOD
+        // $headers = [
+        //     'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+        //     'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
+        // ];
+        // if($request->getMethod() == "OPTIONS") {
+        //     // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+        //     return Response::make('OK', 200, $headers);
+        // }
+        // $response = $next($request);
+        // foreach($headers as $key => $value)
+        //     $response->header($key, $value);
+        // return $response;
     }
 }
