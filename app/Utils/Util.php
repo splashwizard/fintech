c5b117abecbf0f196f9edfb1a355c12b724c55a8
kizzy0216 <m.kizzy.up@gmail.com>
2021-02-08 20:57:31 -0600

    contact_prefix

--- app/Utils/Util.php
+++ app/Utils/Util.php
@@ -315,9 +315,9 @@ class Util
         }
         if (!empty($business_id)) {
             $business = Business::find($business_id);
-//            $prefixes = $business->ref_no_prefixes;
-//            $prefix = $prefixes[$type];
-            $prefix = substr($business->name, 0, 2);
+            $prefixes = $business->ref_no_prefixes;
+            $prefix = $prefixes[$type];
+//            $prefix = substr($business->name, 0, 2);
         }
 
         if (!empty($default_prefix)) {
