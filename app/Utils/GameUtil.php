e0b3c2a1ae27c6c96f33a0ffd7eff92f5efe7d43
kizzy0216 <m.kizzy.up@gmail.com>
2021-04-02 01:21:45 -0500

    Transfer Wallet v1

--- app/Utils/GameUtil.php
+++ app/Utils/GameUtil.php
@@ -57,8 +57,8 @@ class GameUtil extends Util
 //        else if($game_key == 'Transfer Wallet'){ //
         else{
             $result = $this->transferwallet->GetPlayGameUrlWithDepositAmount($username, 0.00, uniqid(),'s6xhiogba5dhe' );
-            \Log::emergency("334343434343434");
-            \Log::emergency($result);
+            \Log::emergency($game_key);
+//            \Log::emergency($result);
             if ($result->Success == true) {
                 $output = ['success' => true, 'link' => $result->ForwardUrl];
             }
