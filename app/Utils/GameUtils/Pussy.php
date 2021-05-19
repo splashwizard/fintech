c1b446da2b03da61e165bdcca91f0e564b375107
Mathew Kizhakkadathu <m.kizzy.up@gmail.com>
2021-05-19 15:27:14 -0500

    test

--- app/Utils/GameUtils/Pussy.php
+++ app/Utils/GameUtils/Pussy.php
@@ -109,6 +109,8 @@ class Pussy
             $ip_address = $util->getUserIpAddr();
             $params = ['action' => "setServerScore", 'scoreNum' => $amount, 'userName' => $account, 'ActionUser' => $username, 'ActionIp' => $ip_address];
             $result = $this->getResponse("ashx/account/setScore.ashx", $account, $params);
+            \Log::emergency("Deposit:" . json_encode($result));
+
             if ($result["success"] == true){
                 $response->Success = true;
                 return $response;
